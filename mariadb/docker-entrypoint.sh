ecb8cc40468e163e27a0e61614ce0ea841c87fe9
Luckman Sugiarto (QuSol) <luckman.sugiarto@qusol.com.au>
2018-03-20 20:15:53 +1100

    Add folder synching for mysql database dump files

--- mariadb/docker-entrypoint.sh
+++ mariadb/docker-entrypoint.sh
@@ -282,12 +282,12 @@ else
 
 	# Bootstrap MySQL
 	log "info" "Setting up root user permissions."
+	echo "CREATE DATABASE qusol ;" | mysql --protocol=socket -uroot
 	echo "DELETE FROM mysql.user ;" | mysql --protocol=socket -uroot
 	echo "CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" | mysql --protocol=socket -uroot
 	echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;" | mysql --protocol=socket -uroot
 	echo "DROP DATABASE IF EXISTS test ;" | mysql --protocol=socket -uroot
-	echo "FLUSH PRIVILEGES ;" | mysql --protocol=socket -uroot
-
+	echo "FLUSH PRIVILEGES ;" | mysql --protocol=socket -uroot	
 
 	# Shutdown MySQL
 	log "info" "Shutting down MySQL."
